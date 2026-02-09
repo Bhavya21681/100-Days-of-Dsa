e40f7585cd99da4d53da052ea8a60409fa7ffe91
Bhavya21681 <Bhavya.22993@stu.upes.ac.in>
2026-02-09 16:56:26 +0530

    Update Q8.c

--- Day 01-10/Q8.c
+++ Day 01-10/Q8.c
@@ -1,11 +1,14 @@
 /*Problem: Given integers a and b, compute a^b using recursion without using pow() function.*/
-a, b = map(int, input().split())
+#include <stdio.h>
 
-def power(a, b):
-    if b == 0:
-        return 1
-    if b == 1:
-        return a
-    return a * power(a, b - 1)
+long long power(int a, int b) {
+    if(b == 0) return 1;
+    return a * power(a, b-1);
+}
 
-print(power(a, b))
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%lld\n", power(a, b));
+    return 0;
+}

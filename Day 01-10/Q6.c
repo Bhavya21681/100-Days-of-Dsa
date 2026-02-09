26cc900c9766f4552430f2ac06cbb006a160b479
Bhavya21681 <Bhavya.22993@stu.upes.ac.in>
2026-02-09 16:55:57 +0530

    Update Q6.c

--- Day 01-10/Q6.c
+++ Day 01-10/Q6.c
@@ -1,14 +1,33 @@
 /*Problem: Given a sorted array of n integers, remove duplicates in-place. Print only unique elements in order.*/
-n = int(input())
-a = list(map(int, input().split()))
+#include <stdio.h>
 
-if n == 0:
-    print()
-else:
-    i = 0
-    for j in range(1, n):
-        if a[j] != a[i]:
-            i += 1
-            a[i] = a[j]
+int main() {
+    int n;
+    scanf("%d", &n);
     
-    print(*a[:i+1])
+    int a[1005];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    if(n == 0) {
+        printf("\n");
+        return 0;
+    }
+    
+    int k = 1;
+    for(int i = 1; i < n; i++) {
+        if(a[i] != a[k-1]) {
+            a[k] = a[i];
+            k++;
+        }
+    }
+    
+    for(int i = 0; i < k; i++) {
+        if(i > 0) printf(" ");
+        printf("%d", a[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}

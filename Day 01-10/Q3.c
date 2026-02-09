6733e007d0ae903282e0d9e1dadac985a3b17ed6
Bhavya21681 <Bhavya.22993@stu.upes.ac.in>
2026-02-09 17:01:20 +0530

    Update Q3.c

--- Day 01-10/Q3.c
+++ Day 01-10/Q3.c
@@ -1,21 +1,16 @@
 /*Problem: Implement linear search to find key k in an array. Count and display the number of comparisons performed.*/
 n = int(input())
-
-arr = list(map(int, input().split()))
-
+a = list(map(int, input().split()))
 k = int(input())
 
-found = False
-comparisons = 0
+count = 0
 
 for i in range(n):
-    comparisons += 1               
-    if arr[i] == k:
-        found = True
-        print(f"Found at index {i}")
-        break
+    count += 1
+    if a[i] == k:
+        print("Found at index", i)
+        print("Comparisons =", count)
+        exit()
 
-if not found:
-    print("Not Found")
-
-print(f"Comparisons = {comparisons}")
+print("Not Found")
+print("Comparisons =", count)

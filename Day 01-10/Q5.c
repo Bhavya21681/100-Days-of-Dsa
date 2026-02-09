2890aa6361d0021dbc38ae4ef8bf433db287e6b0
Bhavya21681 <Bhavya.22993@stu.upes.ac.in>
2026-02-09 16:55:38 +0530

    Update Q5.c

--- Day 01-10/Q5.c
+++ Day 01-10/Q5.c
@@ -1,21 +1,48 @@
 /*Problem: A system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. Your task is to create a single chronological log that preserves the correct order of arrivals.*/
-p = int(input())
-a = list(map(int, input().split()))
-q = int(input())
-b = list(map(int, input().split()))
+#include <stdio.h>
 
-i = j = 0
-result = []
-
-while i < p and j < q:
-    if a[i] <= b[j]:
-        result.append(a[i])
-        i += 1
-    else:
-        result.append(b[j])
-        j += 1
-
-result += a[i:]
-result += b[j:]
-
-print(*result)
+int main() {
+    int p;
+    scanf("%d", &p);
+    int a[1005];
+    for(int i = 0; i < p; i++) scanf("%d", &a[i]);
+    
+    int q;
+    scanf("%d", &q);
+    int b[1005];
+    for(int i = 0; i < q; i++) scanf("%d", &b[i]);
+    
+    int i = 0, j = 0;
+    int first = 1;
+    
+    while(i < p && j < q) {
+        if(a[i] <= b[j]) {
+            if(!first) printf(" ");
+            printf("%d", a[i]);
+            first = 0;
+            i++;
+        } else {
+            if(!first) printf(" ");
+            printf("%d", b[j]);
+            first = 0;
+            j++;
+        }
+    }
+    
+    while(i < p) {
+        if(!first) printf(" ");
+        printf("%d", a[i]);
+        first = 0;
+        i++;
+    }
+    
+    while(j < q) {
+        if(!first) printf(" ");
+        printf("%d", b[j]);
+        first = 0;
+        j++;
+    }
+    printf("\n");
+    
+    return 0;
+})

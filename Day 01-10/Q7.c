804e3f5dafd8eab09573cf2baa7995baf0269f9b
Bhavya21681 <Bhavya.22993@stu.upes.ac.in>
2026-02-09 16:56:11 +0530

    Update Q7.c

--- Day 01-10/Q7.c
+++ Day 01-10/Q7.c
@@ -1,11 +1,14 @@
 /*Problem: Write a recursive function fib(n) to compute the n-th Fibonacci number where fib(0)=0 and fib(1)=1.*/
-n = int(input())
+#include <stdio.h>
 
-def fib(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fib(n-1) + fib(n-2)
+int fib(int n) {
+    if(n <= 1) return n;
+    return fib(n-1) + fib(n-2);
+}
 
-print(fib(n))
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", fib(n));
+    return 0;
+}

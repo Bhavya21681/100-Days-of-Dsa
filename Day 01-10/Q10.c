6d5d8ea062912a4275c86b8926443a7d37e788fe
Bhavya21681 <Bhavya.22993@stu.upes.ac.in>
2026-02-10 16:55:37 +0530

    Create Q10.c

---
+++ Day 01-10/Q10.c
@@ -0,0 +1,30 @@
+/*Problem: Read a string and check if it is a palindrome using two-pointer comparison.*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    
+    scanf("%s", s);
+    
+    int left = 0;
+    int right = strlen(s) - 1;
+    int is_palindrome = 1;
+    
+    while (left < right) {
+        if (s[left] != s[right]) {
+            is_palindrome = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+    
+    if (is_palindrome) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+    
+    return 0;
+}

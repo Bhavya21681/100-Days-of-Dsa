f8552d5243efb178a7648422b084cfe87e41d7b1
Bhavya21681 <Bhavya.22993@stu.upes.ac.in>
2026-02-11 14:26:45 +0530

    Update Q11.c

--- Day 11-20/Q11.c
+++ Day 11-20/Q11.c
@@ -1,1 +1,27 @@
+/*Problem: Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.*/
+#include <stdio.h>
 
+int main() {
+    int m, n, i, j;
+    scanf("%d%d", &m, &n);
+    
+    int a[100][100], b[100][100];
+    
+    for(i = 0; i < m; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+            
+    for(i = 0; i < m; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &b[i][j]);
+            
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            printf("%d", a[i][j] + b[i][j]);
+            if(j < n-1) printf(" ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
